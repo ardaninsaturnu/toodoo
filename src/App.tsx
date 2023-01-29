69c5cff321ad723186b17c462c300ca5644f1690
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-29 11:01:59 +0300

    env files created for config setup. @main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Layout from "./components/layout";
 
 function App() {
+  console.log( process.env.NODE_ENV, process.env.REACT_APP_API_URL )
+
   return (
-   <></>
+   <>
+    <Layout/>
+   </>
   );
 }
 
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return(
+    <div className="container">
+      <button className="button">Alex</button>
+    </div>
+  )
+}
+
+export default Layout;

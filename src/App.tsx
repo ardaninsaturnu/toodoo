e49c55120501388d885c5b32dfa0ba2ae32790c1
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-29 12:32:47 +0300

    env file fixed. @main

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from "./components/layout";
 
 function App() {
-  console.log( process.env.NODE_ENV, process.env.REACT_APP_API_URL )
+  console.log( process.env.NODE_ENV, process.env.REACT_APP_API_URL, process.env )
 
   return (
    <>

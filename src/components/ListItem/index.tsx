9378fe77aca4db971ca3f098f4a8af140df14947
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-02-01 04:10:08 +0300

    completed task done. @main

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -7,28 +7,26 @@ import {removeTodo,updateTodo } from "../../store/slices/todoSlice";
 type PropTypes = {
   task: string,
   id: number,
-  checked: boolean
+  completed: boolean
 }
 
-const ListItem = ({ task, id, checked } : PropTypes ) => {
+const ListItem = ({ task, id, completed } : PropTypes ) => {
   const dispatch = useAppDispatch();
-  const [ completed, setCompleted ] = useState(false);
 
   const handleChange = ( e: SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
     const { checked:status } = target;
     dispatch( updateTodo({ id, status }) );
-    setCompleted( status );
   };
 
   return(
     <div className="list-item">
       <div className="list-item-task">
-        <Input name="status" type="checkbox" onChange={handleChange} checked={checked}/>
-        <p className={`list-item-title ${ checked && 'overLine' }`}>{ task }</p>
+        <Input name="status" type="checkbox" onChange={handleChange} checked={completed}/>
+        <p className={`list-item-title ${ completed && 'overLine' }`}>{ task }</p>
       </div>
       <div className="list-item-transitions">
-        <Trash2 onClick={() => dispatch(removeTodo( id ))} color={ checked ? `grey` : 'red' } style={{ width: 36 }}/>
+        <Trash2 onClick={() => !completed && dispatch(removeTodo( id ))} color={ completed ? `grey` : 'red' } style={{ width: 36 }}/>
       </div>
     </div>
   )
